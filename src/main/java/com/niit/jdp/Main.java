0dfe721c0cbd837aa5f9f13ded822b3c68c5eab9
Pinky Sodhi <pinky.sodhi@niit.com>
2022-11-28 11:01:30 +0530

    created an instance of DatabaseService inside main()

--- src/main/java/com/niit/jdp/Main.java
+++ src/main/java/com/niit/jdp/Main.java
@@ -1,7 +1,9 @@
 package com.niit.jdp;
 
+import com.niit.jdp.service.DatabaseService;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        DatabaseService databaseService = new DatabaseService();
     }
 }
