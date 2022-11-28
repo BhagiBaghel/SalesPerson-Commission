f7e934399b5219cdb1a61b6a474cd323d2c23ab9
Pinky Sodhi <pinky.sodhi@niit.com>
2022-11-28 12:04:14 +0530

    created SalespersonRepository.java

---
+++ src/main/java/com/niit/jdp/repository/SalespersonRepository.java
@@ -0,0 +1,9 @@
+/*
+ * Author : Pinky Sodhi
+ * Date : 28-11-2022
+ * Created Using : IntelliJ IDEA Community Edition
+ */
+package com.niit.jdp.repository;
+
+public class SalespersonRepository {
+}
