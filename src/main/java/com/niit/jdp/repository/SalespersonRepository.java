d6bb1ad48597c4f6db5b1857e0c6d8f478bfb983
Pinky Sodhi <pinky.sodhi@niit.com>
2022-11-28 12:10:04 +0530

    added a no-args constructor in the SalespersonRepository

--- src/main/java/com/niit/jdp/repository/SalespersonRepository.java
+++ src/main/java/com/niit/jdp/repository/SalespersonRepository.java
@@ -8,8 +8,14 @@ package com.niit.jdp.repository;
 import com.niit.jdp.service.DatabaseService;
 
 import java.sql.Connection;
+import java.sql.SQLException;
 
 public class SalespersonRepository {
     Connection connection;
     DatabaseService databaseService;
+
+    public SalespersonRepository() throws SQLException, ClassNotFoundException {
+        databaseService = new DatabaseService();
+        connection = databaseService.getConnectionToDatabase();
+    }
 }
