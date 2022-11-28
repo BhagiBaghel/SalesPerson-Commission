c86f71eefcf73eac8656a12eb5f3dc1871f311b5
Pinky Sodhi <pinky.sodhi@niit.com>
2022-11-28 15:45:16 +0530

    modified the getAllSalesperson()

--- src/main/java/com/niit/jdp/repository/SalespersonRepository.java
+++ src/main/java/com/niit/jdp/repository/SalespersonRepository.java
@@ -45,7 +45,7 @@ public class SalespersonRepository {
                 double commissionRate = resultSet.getDouble("commission_rate");
 
                 //7. create a Salesperson object using the values from the result set
-                Salesperson salesperson = new Salesperson(name, city, commissionRate);
+                Salesperson salesperson = new Salesperson(salesId, name, city, commissionRate);
 
                 //8. add the salesperson object to the salespersons list
                 salespersons.add(salesperson);
